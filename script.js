c9c32434e206e3901b77da4953d7abe9c06c64b1
oopapenko <79106390+oopapenko@users.noreply.github.com>
2021-02-17 22:01:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,30 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-let todos = [];
-let id = 0;
+var todos = [];
+try {
+  var len = localStorage.getItem('length');
+  console.log("len="+len)
+  let todo=[];
+  if(len>0){
+    for(let i = 0; i<len;i++){
+      if(localStorage.getItem(`todo.id${i}`)===undefined){
+        break;
+      }      
+      todo = {id: Number(localStorage.getItem(`todo.id${i}`)), check: Boolean(localStorage.getItem(`todo.check${i}`)),text: localStorage.getItem(`todo.text${i}`)};      
+      todos.push(todo);         
+    }
+  }
+  var id=todos[len-1].id;
+  console.log('id='+id)
+}
+catch (e) {
+  var id = 0;
+}
+if (todos.length != 0) {
+  render();
+}
+
 
 //<li>
 //  <input type="checkbox">
@@ -18,46 +40,66 @@ let id = 0;
 //  <span>text</span>
 //</li>
 
-class Todo{
-  constructor(){
-    this.id=++id;
+class Todo {
+  constructor() {
+    this.id = ++id;
     this.check = false;
     this.text = this.getText();
   }
-  getText(){
+  getText() {
     return prompt('Enter a todo task:')
   }
 }
 
-function newTodo() {  
+function newTodo() {
   const todo = new Todo();
   todos.push(todo);
   render();
 }
 
-function render(){
-  list.innerHTML=``;
+function render() {
+  list.innerHTML = ``;
   todos.map(createTodo).forEach(todo => list.appendChild(todo));
-  itemCountSpan.textContent=todos.length;
+  localStorage.setItem('length', todos.length)
+  for(let i = 0; i<todos.length;i++){  
+    localStorage.setItem(`todo.id${i}`,todos[i].id);    
+    localStorage.setItem(`todo.check${i}`,todos[i].check?1:"");    
+    localStorage.setItem(`todo.text${i}`,todos[i].text);    
+  }
+  
+  itemCountSpan.textContent = todos.length;
+  uncheckedCountSpan.textContent = todos.filter(todo => !todo.check).length;
 }
 
-function createTodo(todo){
-  //create li
+function createTodo(todo) {
   const li = document.createElement("li");
-
-  //create input checkbox
-  //create button
-  //create span
-
+  li.className = 'todo-container'
   li.innerHTML = `
-  <input type="checkbox">
-  <button onclick="deleteTodo(${todo.id})">delete</button>
+  <input class = 'todo-checkbox' type="checkbox" onchange="changeTodo(${todo.id})" ${todo.check ? "checked" : ""}>
+  <button class = 'todo-delete' onclick="deleteTodo(${todo.id})">delete</button>
   <span>${todo.text}</span>
   `;
   return li;
 }
 
-function deleteTodo(id){
+function deleteTodo(id) {
   todos = todos.filter(todo => todo.id !== id)
-  render();  
+  render();
+}
+
+function changeTodo(id) {
+  //  for (let i = 0; i < todos.length; i++) {
+  //    if (todos[i].id === id) {
+  //      todos[i].check = !todos[i].check;
+  //      break;
+  //    }
+  //  }
+
+
+  //todos = todos.map(todo => todo.id === id ? { id: todo.id, text: todo.text, check: !todo.check } : todo)
+
+  todos = todos.map(todo => todo.id === id ? { ...todo, check: !todo.check } : todo)
+
+  uncheckedCountSpan.textContent = todos.filter(todo => !todo.check).length;
+  render()
 }
